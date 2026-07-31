db91ca9ef857bb53ff6abaa8b2d90e2bf5b49315
Saify <Saify@SAIF>
2026-07-31 21:11:32 +0300

    fox cors

--- backend/src/main/java/com/example/valet/config/CorsConfig.java
+++ backend/src/main/java/com/example/valet/config/CorsConfig.java
@@ -1,12 +1,15 @@
 package com.example.valet.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry r) {
-        r.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+        r.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://2.28.16.126")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
